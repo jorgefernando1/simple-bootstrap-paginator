d40f249fee3bb4c9f974cb971403b8cfa3fd9666
Jorge Fernando Alves Feitoza <jorge.fernando1@live.com>
2015-12-29 12:26:49 -0200

    html demo created

---
+++ simple-bootstrap-paginator.js
@@ -0,0 +1,11 @@
+(function($, window) {
+
+  /**
+  * Simple boostrap pagination v 0.1
+  */
+  $.fn.simplePagination = function(options) {
+    $(this).text('First version works!');
+  }
+
+
+})(jQuery, window);
